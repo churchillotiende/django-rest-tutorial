bfc4f6b92c29d12f5610ad834fece8990319bd63
churchillotiende <churchillotiende84@gmail.com>
2025-04-21 09:35:51 +0300

    The client files for crud testing

---
+++ client/create.py
@@ -0,0 +1,14 @@
+import requests
+
+endpoint = "http://localhost:8000/api/products/"
+
+data = {
+        "title":"This field is done",
+        "price":35.50,
+        }
+
+get_response = requests.post(endpoint,json=data)
+print(get_response.json())
+
+
+
---
+++ client/delete.py
@@ -0,0 +1,19 @@
+import requests
+
+product_id = input("Enter the product id you would like to use \n")
+
+try:
+    product_id = int(product_id)
+except:
+    product_id = None
+
+    print(f'{product_id} is not valid')
+
+if product_id:
+    endpoint = f"http://localhost:8000/api/products/{product_id}/delete"
+
+
+
+get_response = requests.delete(endpoint)
+
+print(get_response.status_code,get_response.status_code == 204)
