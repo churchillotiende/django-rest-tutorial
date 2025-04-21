527c21c239bf0cd54b910dfb528331107960aaf6
churchillotiende <churchillotiende84@gmail.com>
2025-04-21 09:35:03 +0300

    Successfully completed crud on the products module

--- django_project/products/urls.py
+++ django_project/products/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/',views.ProductDetailAPIView.as_view())
-]
+
+        path('<int:pk>/',views.ProductDetailAPIView.as_view()),
+        path('<int:pk>/',views.ProductDetailAPIView.as_view()),
+        path('<int:pk>/update',views.product_update_view),
+        path('<int:pk>/delete',views.product_destroy_view),
+        path('',views.product_list_view),
+        ]
--- django_project/products/views.py
+++ django_project/products/views.py
@@ -8,12 +8,41 @@ class ProductListView(generics.ListCreateAPIView):
    List all products, or create a new one.
   """
   model = Product
+  queryset = Product.objects.all()
+
   serializer_class = ProductSerializer
+
+product_list_view = ProductListView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     lookup_field = "pk"
     queryset = Product.objects.all()
     serializer_class  =ProductSerializer
+
+#Update view
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    lookup_field = "pk"
+    queryset = Product.objects.all()
+    serializer_class  =ProductSerializer
+
+    def perform_update(self,serializer):
+        instance = serializer.save()
+        if not instance.content:
+            istance.content = instance.title
+
+product_update_view = ProductUpdateAPIView.as_view()
+
+#destroy view
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    lookup_field = "pk"
+    queryset = Product.objects.all()
+    serializer_class  =ProductSerializer
+
+    def perform_destroy(self,instance):
+       #instance
+        super().perform_destroy(instance)
+
+product_destroy_view = ProductDestroyAPIView.as_view()
 
 class ProductCreateAPIView(generics.CreateAPIView):
     queryset = Product.objects.all()
